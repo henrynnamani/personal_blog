1736c0d2baa93daf50e77476cd3d655ea6f0fd66
gr4yx01 <hoyx0101@gmail.com>
2025-09-15 23:12:35 +0100

    feat: working

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 import express from 'express'
 import session from 'express-session';
+import guestRouter from './routes/guest.js'
 import path from 'path'
 import { fileURLToPath } from 'url';
+import fs from 'fs'
 
 const app = express();
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+
+const filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(filename)
 
 app.use(express.urlencoded())
 app.use(session({
@@ -15,7 +18,38 @@ app.use(session({
 }))
 
 app.set('view engine', 'ejs')
-app.set('views', path.join(__filename, 'views'))
+app.set('views', path.join(__dirname, 'views'))
+
+app.get('/', (req, res) => res.redirect('/home'))
+
+app.get('/home', (req, res) => { // default url -> /
+    fs.readFile('articles/index.json', (err, data) => {
+        if(err) {
+            console.log('Error reading file...')
+        }
+
+        const articles = JSON.parse(data);
+
+        return res.render('guest/index', { articles, title: 'List of articles' })
+    })
+})
+
+app.get('/articles/:id', (req, res) => {
+    const { id } = req.params;
+
+    fs.readFile('articles/index.json', (err, data) => {
+        if(err) {
+            console.log('Error reading file...')
+        }
+
+        const articles = JSON.parse(data)
+
+        const article = articles.filter((article) => article.id == Number(id) )[0]
+
+        return res.render('guest/article', { article, title: 'Article content' })
+    })
+})
+
 
 app.listen(3000, () => {
     console.log('Server is running')
